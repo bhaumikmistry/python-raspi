ef95d64f198b04c3b5a70cfd7130364aebe643e5
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2017-10-28 16:27:38 -0400

    adding bird

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 from sense_hat import SenseHat
+from sense_hat import ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+
 from time import sleep
 import pygame  # See http://www.pygame.org/docs
 from pygame.locals import *
 
+xbird = 3
+ybrid = 3
 
 # r = [255, 0, 0]
-# g = [0,255,0]
+g = [0,255,0]
 # w = [255, 255, 255]
 
 def get_names():
@@ -23,6 +27,27 @@ def get_names():
     ]
     return names
 
+def pushed_up(event):
+    global ybrid
+    if event.action != ACTION_RELEASED:
+        print("up pressed ----->>")
+        sense.set_pixel(xbird, ybrid, g)                      
+        ybrid -= 1
+
+def pushed_down(event):
+    global ybrid
+    if event.action != ACTION_RELEASED:
+        print("up pressed ----->>")    
+        sense.set_pixel(xbird, ybrid, g)                          
+        ybrid += 1
+        
+def setBird(sense):
+    sense.set_pixel(0, 0, red)
+
+
+sense.stick.direction_up = pushed_up
+sense.stick.direction_down = pushed_down
+
 if __name__ == "__main__":
     sense = SenseHat()
     sense.set_rotation(180)
@@ -32,20 +57,28 @@ if __name__ == "__main__":
             print('"{}"'.format(x))
         sense.clear()
         sense.load_image("stage1.png")
+        sense.set_pixel(xbird, ybrid, g)              
         sleep(0.7) # Time in seconds.
         sense.load_image("stage2.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage3.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage4.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage5.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage6.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage7.png")
+        sense.set_pixel(xbird, ybrid, g)                      
         sleep(0.7) # Time in seconds.
         sense.load_image("stage8.png")
+        sense.set_pixel(xbird, ybrid, g)    
         sleep(0.7) # Time in seconds.
 
 
