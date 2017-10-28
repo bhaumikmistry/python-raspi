c96ad29739888e5049e1942f0ad641f61107e8e8
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2017-10-28 16:46:56 -0400

    change format

--- src/main.py
+++ src/main.py
@@ -45,41 +45,94 @@ def pushed_down(event):
 def setBird(sense):
     sense.set_pixel(0, 0, red)
 
+def check_bird():
+    if ybrid > 7 && ybrid < 0:
+        return False
 
 sense.stick.direction_up = pushed_up
 sense.stick.direction_down = pushed_down
 
-if __name__ == "__main__":
-    sense.set_rotation(180)
-    list_name = get_names()
+def boom():
+    sense.load_image("l0_boom1.png")
+    sleep(0.3) # Time in seconds.
+    sense.load_image("l0_boom2.png")
+    sleep(0.3) # Time in seconds.
+    sense.load_image("l0_boom3.png")
+    sleep(0.3) # Time in seconds.
+    sense.load_image("l0_boom4.png")
+    sleep(0.3) # Time in seconds.
+    sense.load_image("l0_boom5.png")
+    sleep(0.3) # Time in seconds.
+    sense.load_image("l0_boom6.png")
+    sleep(0.3) # Time in seconds.
+    
+
+def main_unit():
     while True:
+        list_name = get_names()
         for x in list_name:
             print('"{}"'.format(x))
         sense.clear()
+
         sense.load_image("stage1.png")
-        sense.set_pixel(xbird, ybrid, g)              
+        sense.set_pixel(xbird, ybrid, g)     
+        res = check_bird()
+        if(!res):
+            break
         sleep(0.7) # Time in seconds.
         sense.load_image("stage2.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g)    
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage3.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g)  
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage4.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g)
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage5.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g)
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage6.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g)  
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage7.png")
-        sense.set_pixel(xbird, ybrid, g)                      
+        sense.set_pixel(xbird, ybrid, g) 
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
         sense.load_image("stage8.png")
-        sense.set_pixel(xbird, ybrid, g)    
+        sense.set_pixel(xbird, ybrid, g)
+        if(!res):
+            break
+
         sleep(0.7) # Time in seconds.
+    return True
+
+
+if __name__ == "__main__":
+    sense.set_rotation(180)
+    while True:
+        main_unit()
+        boom()
+        global ybrid = 3
+
+
 
 
 
